2b8f9a5390824657e5e1cdf56e686d845a357fd7
Wilson Tran <wt.0317@gmail.com>
2014-05-30 12:24:22 -0400

    fix up code as per email, comments, and references

--- Assignment1/source/client_stub.c
+++ Assignment1/source/client_stub.c
@@ -1,3 +1,5 @@
+// google-stylguide
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,10 +7,11 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include "ece454rpc_types.h"
-#include "mybind.c"
 #include <stdarg.h>
 
 #define BUFLEN 2048
+
+return_type FAILED = { NULL, 0 };
 
 return_type make_remote_call(const char *servernameorip, 
 							const int serverportnumber,
@@ -20,13 +23,15 @@ return_type make_remote_call(const char *servernameorip,
 	char buf[BUFLEN];	/* message buffer */
 	int recvlen;		/* # bytes in acknowledgement message */
 	struct hostent *he;	/* used to get ip from hostname */
-	int bufferSize, seeker, procedureNameSize;
+	int buffer_size, seeker, procedure_name_size;
 	va_list list;
 	char *buffer;
 
 	/* create a socket */
-	if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1)
+	if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1) {
 		perror("socket not created\n");
+		return FAILED;
+	}
 
 	/* bind it to all local addresses and pick any port number */
 	memset((char *)&myaddr, 0, sizeof(myaddr));
@@ -36,16 +41,14 @@ return_type make_remote_call(const char *servernameorip,
 
 	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
 		perror("bind failed");
-		return;
+		return FAILED;
 	}       
 
 	/* now define remaddr, the address to whom we want to send messages */
-	/* For convenience, the host address is expressed as a numeric IP address */
-	/* that we will convert to a binary format via inet_aton */
 
 	/* resolve hostname */
 	if ( (he = gethostbyname(servernameorip) ) == NULL ) {
-		exit(1); /* error */
+		return FAILED;
 	}
 
 	memcpy(&remaddr.sin_addr, he->h_addr_list[0], he->h_length);
@@ -53,33 +56,40 @@ return_type make_remote_call(const char *servernameorip,
 	remaddr.sin_port = htons(serverportnumber);
 
 	/* now let's send the messages */
+	// determine size of arg list to allocate buffer
 	va_start(list, nparams);
-	bufferSize = 0;	
+	buffer_size = 0;	
 	for (i = 0; i < nparams; i++) {
 		int arg_size = va_arg(list, int);
-		bufferSize += sizeof(arg_size);
-		bufferSize += arg_size;
+		buffer_size += sizeof(arg_size);
+		buffer_size += arg_size;
 		va_arg(list, void*);
 	}
 	va_end(list);
 
-	procedureNameSize = sizeof(procedure_name);
-	bufferSize += sizeof(procedureNameSize) + procedureNameSize + sizeof(nparams);
+	// account for size of sending procedure name and nparams
+	procedure_name_size = sizeof(procedure_name);
+	buffer_size += sizeof(procedure_name_size) + procedure_name_size + sizeof(nparams);
 	/* allocate memory for the list, and let 'buffer' point to it. */
-  	buffer = (char *)malloc(bufferSize);
+  	buffer = (char *)malloc(buffer_size);
   	seeker = 0;
-  	memcpy(&buffer[seeker], &procedureNameSize, sizeof(procedureNameSize));
-  	seeker += sizeof(procedureNameSize);
-  	memcpy(&buffer[seeker], procedure_name, procedureNameSize);
-  	seeker += procedureNameSize;
+  	// copy bytes of procedure name into buffer
+  	memcpy(&buffer[seeker], &procedure_name_size, sizeof(procedure_name_size));
+  	seeker += sizeof(procedure_name_size);
+  	// copy procedure name into buffer
+  	memcpy(&buffer[seeker], procedure_name, procedure_name_size);
+  	seeker += procedure_name_size;
+  	// copy nparams into buffer
   	memcpy(&buffer[seeker], &nparams, sizeof(nparams));
   	seeker += sizeof(nparams);
 
+  	// copy args into buffer
 	va_start(list, nparams);
 	for (i = 0; i < nparams; i++) {
 		int arg_size = va_arg(list, int);
 		void* arg_val = va_arg(list, void*);
 
+		// put the size of the arg into the buffer
 		memcpy(&buffer[seeker], &arg_size, sizeof(arg_size));
 		seeker += sizeof(arg_size); /* move seeker ahead by 4 bytes */
 
@@ -89,9 +99,9 @@ return_type make_remote_call(const char *servernameorip,
 	}
 	va_end(list);
 
-	if (sendto(fd, buffer, bufferSize, 0, (struct sockaddr *)&remaddr, slen)==-1) {
+	if (sendto(fd, buffer, buffer_size, 0, (struct sockaddr *)&remaddr, slen)==-1) {
 		perror("sendto");
-		exit(1);
+		return FAILED;
 	}
 	/* now receive an acknowledgement from the server */
 	return_type returnedVal;
--- Assignment1/source/server_stub.c
+++ Assignment1/source/server_stub.c
@@ -1,3 +1,5 @@
+// google-stylguide
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -21,16 +23,21 @@ int func_db_count = 0;
 
 bool register_procedure(const char *procedure_name, const int nparams, fp_type fnpointer)
 {
+	if (func_db_count > DBSIZE) {
+		return false;
+	}
+
 	func_db[func_db_count].procedure_name = procedure_name;
 	func_db[func_db_count].nparams = nparams;
 	func_db[func_db_count].fnpointer = fnpointer;
 	func_db_count++;
+	return true;
 }
 
 arg_type *start;
 arg_type *end;
 
-/* function to add 'item' to a linked list */
+/* function to add 'item' to a linked list, from http://lnxdev.com/view_article.php?id=89 */
 void addToList(int arg_size, void **buffer)
 {
 	arg_type *ptr;                 /* creating a pointer */
@@ -40,7 +47,7 @@ void addToList(int arg_size, void **buffer)
 		start = ptr;              /* structure pointed by ptr is first in the list */
 		ptr->next = 0;            /* next is 0, this is also the last item in the list */
 	}
-	else {                      /* if there are allready items in the list */
+	else {                      /* if there are already items in the list */
 		end->next = ptr;          /* last item in the list points to this. */
 	}
 	end = ptr;                  /* last item in the list is ptr */
@@ -89,53 +96,70 @@ void launch_server()
 	for (;;) {
 		recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
 		if (recvlen > 0) {
-			int done = 0;
-			int arg_size;
-			int procedureNameSize;
+			int done = 0; // tracking index of received buffer
+			
+			int procedure_name_size;
 			char* procedure_name;
 			int nparams;
+			int arg_size;
+			void** arg_val; // temporarily hold arg_val
 
-			void** buffer;
-
-			memcpy(&procedureNameSize, &buf[done], sizeof(procedureNameSize));
-			done += sizeof(procedureNameSize);
-			procedure_name = (char *)malloc(procedureNameSize);
-			memcpy(procedure_name, &buf[done], procedureNameSize);
-			done += procedureNameSize;
+			// determine the number of bytes of the procedure name
+			memcpy(&procedure_name_size, &buf[done], sizeof(procedure_name_size));
+			done += sizeof(procedure_name_size);
+			// allocate memory and read procedure name into procedure_name
+			procedure_name = (char *)malloc(procedure_name_size);
+			memcpy(procedure_name, &buf[done], procedure_name_size);
+			done += procedure_name_size;
+			// read number of params from received buffer
 			memcpy(&nparams, &buf[done], sizeof(nparams));
 			done += sizeof(nparams);
 
-			start = 0;
+			// re-initialize pointers for linked list of args
+			start = 0; 
 			end = 0;
 
 			while (done < recvlen) {
+				//determine the number of bytes of the arg_val
 				memcpy(&arg_size, &buf[done], sizeof(arg_size)); 
 				done += sizeof(arg_size);
-				buffer = (void *)malloc(arg_size); /* allocate space for arg_val */
-				memcpy(buffer, &buf[done], arg_size);  /* read arg_size amount of bytes from buf */
-				addToList(arg_size, buffer);          /* adding arg_size and arg_val to linked list */
-				done += arg_size;                 /* done keeps track of amount of data red from buf */
-				free(buffer);
+
+				arg_val = (void *)malloc(arg_size); /* allocate space for arg_val */
+				memcpy(arg_val, &buf[done], arg_size);  /* read arg_size amount of bytes from buf */
+				addToList(arg_size, arg_val);          /* adding arg_size and arg_val to linked list */
+				done += arg_size;                 /* done keeps track of amount of data read from buf */
+				free(arg_val);
 			}
 
+			// find the function to call in func_db
+			f = 0;
 			for (i = 0; i < DBSIZE; i++) {
-				if (strcmp(func_db[i].procedure_name,procedure_name) == 0) {
+				if (strcmp(func_db[i].procedure_name, procedure_name) == 0) {
 					f = (fp_type)func_db[i].fnpointer;
 					break;
 				}
 			}
-			return_type result = f(2, start);
+			return_type result;
+			if (f==0) {
+				result.return_val = NULL;
+				result.return_size = 0;
+			} else {
+				result = f(nparams, start);
+			}
 			
+			// copy result into a buffer of bytestream to return to client
 			sizeOfReturnBuf = result.return_size + sizeof(result.return_size);
 			returnBuf = (char *)malloc(sizeOfReturnBuf);
 			memcpy(returnBuf, &result.return_size, sizeof(result.return_size));
 			memcpy(&returnBuf[sizeof(result.return_size)], result.return_val, result.return_size);
+
+			if (sendto(fd, returnBuf, sizeOfReturnBuf, 0, (struct sockaddr *)&remaddr, addrlen) < 0) {
+				perror("sendto");
+			}
+
+			free(start);
+			free(end);
 		}
-		else
-			printf("uh oh - something went wrong!\n");
-		// printf("sending response \"%s\"\n", buf);
-		if (sendto(fd, returnBuf, sizeOfReturnBuf, 0, (struct sockaddr *)&remaddr, addrlen) < 0)
-			perror("sendto");
 	}
 	/* never exits */
 }
